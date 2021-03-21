d8d69f7932817a6e2b58389db30772a2f1109a92
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-03-21 02:29:18 -0700

    Update primes_opt.c

--- TP1/EX3/primes_opt.c
+++ TP1/EX3/primes_opt.c
@@ -8,11 +8,26 @@
 
 static bool    is_prime_opt(int n)
 {
-    // write is_prime function code here
+    if(n == 2)
+        return 1;
+    if(n < 2 || n%2==0)
+        return 0;
+    for(int i = 3; i<= sqrt(n); i=i+2)
+    {
+        if(n%i == 0)
+            return 0;
+    }
+    return 1;
 }
 
-
-void    get_primes_opt(int n)
+int get_primes_opt(int n)
 {
-    // write get_primes_opt function code here that use is_prime_opt function
+    if(n>2)
+        print(2);
+    for(int i = 3; i<=n; i+=2)
+    {
+        if(is_prime_opt(i))
+            print("%d is prime\n", i);
+
+    }
 }
