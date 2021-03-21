e13e4e759f8ae00542d25f48fad2c03e89d5e2ff
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-03-21 02:51:06 -0700

    Update primes_opt.c

--- TP1/EX3/primes_opt.c
+++ TP1/EX3/primes_opt.c
@@ -23,11 +23,11 @@ static bool    is_prime_opt(int n)
 void get_primes_opt(int n)
 {
     if(n>2)
-        print(2);
+        printf("2");
     for(int i = 3; i<=n; i+=2)
     {
         if(is_prime_opt(i))
-            print("%d is prime\n", i);
+            printf("%d is prime\n", i);
 
     }
 }
