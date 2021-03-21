475de9a7024678ccc9efcf9067fe42b1228a7434
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-03-21 02:05:30 -0700

    Update iterative_fibo.c

--- TP1/EX1/iterative_fibo.c
+++ TP1/EX1/iterative_fibo.c
@@ -2,5 +2,14 @@
 
 int iterative_fibo(int n)
 {
-    // write your iterative_fibo code here
+    int resultat = 0, nbr1 = 0, nbr2 = 1;
+    if(n == 0) {return 0;}
+    if(n == 1) {return 1;}
+    for(int i = 0; i <= n; i++)
+    {
+        resultat = nbr1 + nbr2;
+        nbr1 = nbr2;
+        nbr2 = resultat;
+    }
+    return resultat;
 }
