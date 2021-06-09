53e7f1d67583891e031a384dcf3ce52e73f0e9af
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-06-09 02:00:05 +0100

    Update TP3-EX3.c

--- TP3/TP3-EX3.c
+++ TP3/TP3-EX3.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 
 struct s_node
 {
@@ -25,26 +26,36 @@ t_node* push(t_node* head, int data)
     return head;
 }
 //Pop an element from the satck
-t_node* pop(t_node* head, int* element)//I don't understand why we used an int pointer
+int pop(t_node* head)
 {
+    if(head == NULL)
+        return INT_MIN;
     t_node* temp = head;
     head = head->next;
-    *element = head->data;
+    int element = head->data;
     free(temp);
-    return head;
+    return element;
+}
+//Return the top elemnt in the stack
+int peek_stack(t_node* head)
+{
+    if(head == NULL)
+        return INT_MIN;
+    int element = head->data;
+    return element;
 }
 //Display the stack content
 void display(t_node* head)
 {
     if(head == NULL)
     {
-        return NULL;
+        return;
     }
     t_node* temp = head;
     printf("Stack : ");
     while(temp != NULL)
     {
-        printf("%d", temp->data);
+        printf("%d  ", temp->data);
         temp = temp->next;
     }
     printf("\n");
@@ -60,8 +71,8 @@ int main()
     int n_elements = 0;
     int element = 0;
     int counter = 0;
-    inti_stack(head);
-    printf("Enter the the number of elments of the linked stack: ");
+    initi_stack(head);
+    printf("Enter the the number of elements of the linked stack: ");
     scanf("%d", &n_elements);
     printf("---Push elements into the stack---\n");
     while(counter < n_elements)
@@ -72,12 +83,8 @@ int main()
         display(head);
         counter++;
     }
-    printf("Pop elments from the linked stack");
-    while(empty(head) != 0)
-    {
-        printf("Pop %d from stack :", element);
-        head = pop(head, &element);
-        display(head);
-    }
+    printf("---Pop the top element from the linked stack---");
+    element = pop(head);
+    //display(head);
     return 0;
 }
